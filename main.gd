bf61f7c4b1baa1e1fde727c793e953dd1d385d57
Ethan <ethanfrancis577@gmail.com>
2024-08-12 14:05:23 -0400

    made the spikes work

--- main.gd
+++ main.gd
@@ -1,15 +1,26 @@
 extends Node2D
-
+@export var spikeScene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$Camera2D/CanvasLayer/ScoreLabel.text = str(Global.score)
 	Global.enterPos = $enterPipe.position
 
 
 func _on_exit_pipe_body_entered(body):
-	$Player.position = Global.enterPos
+	if body.is_in_group("player"):
+		Global.score += 1 
+		$Player.position = Global.enterPos
+		if Global.score >= 1:
+			var s = spikeScene.instantiate()
+		
+			var spike_spawn_location = $SpikePath/SpikeLocation
+			spike_spawn_location.progress_ratio = randf()
+		
+			s.position = spike_spawn_location.position
+			get_parent().call_deferred("add_child", s)
